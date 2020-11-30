7fa4d498a7bc848a59e4585b26a67c44cedd4ef1
SamuelXSS <samuelxses@unipam.edu.br>
2020-11-30 14:37:01 -0300

    backend: added CPF & CNPJ validation

--- app/src/app.py
+++ app/src/app.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, json
+from flask_cors import CORS, cross_origin
 import datetime
-from CpfValidation import ValidateCpf
+from validate_docbr import CPF
+from validate_docbr import CNPJ
 
 app = Flask(__name__)
+
+app.debug = True
+
+cors = CORS(app)
 
 @app.route('/health')
 def index():
@@ -19,11 +25,12 @@ def index():
     return health
 
 @app.route('/validator/Cpf', methods=['POST'])
-def store():
-    cpfNumber = request.get_json('cpf')
-    cpf = ValidateCpf(cpfNumber)
+def checkCpf():
+    data = request.get_json()
+    cpfNumber = data.get('cpf')
+    cpf = CPF()
 
-    if cpf.validate:
+    if cpf.validate(cpfNumber):
         return {
             "status": 200,
             "cpf": cpfNumber,
@@ -36,6 +43,27 @@ def store():
             "cpf": cpfNumber,
             "code": "error",
             "data": "CPF is not valid"
+        }
+
+@app.route('/validator/Cnpj', methods=['POST'])
+def checkCnpj():
+    data = request.get_json()
+    cnpjNumber = data.get('cnpj')
+    cnpj = CNPJ()
+
+    if cnpj.validate(cnpjNumber):
+        return {
+            "status": 200,
+            "cpf": cnpjNumber,
+            "code": "success",
+            "data": "CNPJ is valid"
+        }
+    else:
+        return {
+            "status": 406,
+            "cpf": cnpjNumber,
+            "code": "error",
+            "data": "CNPJ is not valid"
         }
 
 
